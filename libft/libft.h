4c9f86215a71f79b9b7787eef9127ce66fcc3529
lalaloka <tafftin@gmail.com>
2020-10-25 13:39:02 +0300

    all funct, no bonus

--- libft/libft.h
+++ libft/libft.h
@@ -58,6 +58,7 @@ void	ft_memdel(void **ap);
 void	ft_strclr(char *s);
 void	ft_strdel(char **as);
 void	ft_striter(char *s, void (*f)(char *));
+void    ft_striteri(char *s, void (*f)(unsigned int, char *));
 char	*ft_strmap(char const *s, char (*f)(char));
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 
